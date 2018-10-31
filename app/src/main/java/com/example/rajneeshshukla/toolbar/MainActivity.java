b4b4b791c44d03441d2c56c191e52e6ed21142be
Rajneesh Shukla <rajneesh9651@gmail.com>
2018-10-31 12:07:22 +0530

    How to save data in shared preferences

--- app/src/main/java/com/example/rajneeshshukla/toolbar/MainActivity.java
+++ app/src/main/java/com/example/rajneeshshukla/toolbar/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.rajneeshshukla.toolbar;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("One");
     }
 
-    public void starNewActivity(View view) {
-        startActivity(new Intent(this, ActivityTwo.class));
+    public void saveData(View view) {
+        SharedPreferences preferences = getSharedPreferences("MY_DATA", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("MY_TEXT", "This is the name of the coundtry");
+        editor.apply();
+        Toast.makeText(this, "Data is saved", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void logData(View view) {
+        SharedPreferences preferences = getSharedPreferences("MY_DATA", MODE_PRIVATE);
+        String s = preferences.getString("MY_TEXT", "Did not save any data");
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
